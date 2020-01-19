dfe78e428ee429cb6d729c136cc407cb47264a32
Hunter Treadaway <huntertreadaway@Hunters-MacBook-Pro.local>
2020-01-19 09:41:39 -0800

    updated user actions/reducers/selectors

--- js/redux/user/user.reducer.js
+++ js/redux/user/user.reducer.js
@@ -1,44 +1,44 @@
 const INITIAL_STATE = {
-  displayName: '',
+  id: ''
   email: '',
-  password: '', // assuming we dont actually want to store this in state? not sure
-  profilePic: '',
-  tours: [],
-  userId: ''
+  username: '',
+  pw: '',
+  profile_pic_url: '',
+  created_tours: [],
 }
 
 const UserReducer = (state=INITIAL_STATE, action) => {
   switch (action.type) {
-    case 'SET_USER_DISPLAY_NAME':
+    case 'SET_USER_ID':
       return ({
         ...state,
-        displayName: action.payload
+        id: action.payload
       });
     case 'SET_USER_EMAIL':
       return ({
         ...state,
         email: action.payload
       });
+    case 'SET_USER_NAME':
+      return ({
+        ...state,
+        username: action.payload
+      })
     case 'SET_USER_PASSWORD':
       return ({
         ...state,
-        password: action.payload
+        pw: action.payload
       });
     case 'SET_USER_PROFILE_PIC':
       return ({
         ...state,
-        profilePic: action.payload
+        profile_pic_url: action.payload
       });
-    case 'SET_USER_TOUR':
+    case 'SET_USER_CREATED_TOURS':
       return ({
         ...state,
-        tours: action.payload
+        created_tours: action.payload
       });
-    case 'SET_USER_ID':
-      return ({
-        ...state,
-        userId: action.payload
-      })
     default:
       return state;
   }
