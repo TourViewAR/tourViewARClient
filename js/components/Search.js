470489f5f0b6fe88660cdcfbd7d67c9f390c9dc0
Hunter Treadaway <huntertreadaway@Hunters-MacBook-Pro.local>
2020-01-20 15:25:35 -0800

    includes non-functional search route, need to debug

--- js/components/Search.js
+++ js/components/Search.js
@@ -1,7 +1,6 @@
 import React, { Component, useState, useCallback } from "react";
 import { ScrollView, View, StyleSheet, Image, TextInput } from "react-native";
 import axios from "axios";
-
 import TourContainer from "./TourContainer";
 import { connect } from "react-redux";
 import { navigate } from "../redux/render/render.action";
@@ -9,9 +8,6 @@ import {
   Container,
   Header,
   Content,
-  InputGroup,
-  Input,
-  Icon,
   Text,
   Left,
   Body,
@@ -20,11 +16,9 @@ import {
   Title
 } from "native-base";
 import Axios from "axios";
-
 const Search = props => {
   const [search, setSearch] = useState("");
   const [searchResults, setSearchResults] = useState([]);
-
   const searchRequest = useCallback(() => {
     axios
       .get(
@@ -41,9 +35,8 @@ const Search = props => {
         }
       )
       .then(results => {
-        setSearchResults(results.data.rows);
-
-        alert("Success");
+        setSearchResults(JSON.stringify(results.data));
+        // alert(JSON.stringify(results.data));
       })
       .catch(err => {
         alert(err);
@@ -103,16 +96,18 @@ const Search = props => {
             <Text>Search</Text>
           </Button>
         </View>
-        {/* <ScrollView style={localStyles.container}>
-          {searchResults.map((prop, i) => (
-            <TourContainer key={i} tour={prop} />
+        <ScrollView style={localStyles.container}>
+          {searchResults[0].map((tour, i) => (
+            <TourContainer key={i} tour={tour} />
           ))}
-        </ScrollView> */}
+        </ScrollView>
+        <View>
+          <Text>{`${searchResults}`}</Text>
+        </View>
       </Content>
     </Container>
   );
 };
-
 const localStyles = StyleSheet.create({
   container: {
     flex: 1,
@@ -125,11 +120,9 @@ const localStyles = StyleSheet.create({
     borderRadius: 10
   }
 });
-
 const mapDispatchToProps = dispatch => {
   return {
     navigate: render => dispatch(navigate(render))
   };
 };
-
 export default connect(null, mapDispatchToProps)(Search);
